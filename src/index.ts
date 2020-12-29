52045aa038c0f2813f3e65db0bd04230e553d112
Selena Siri <selenasiri@gmail.com>
2020-12-29 09:14:44 -0500

    /login

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express'
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send(`
